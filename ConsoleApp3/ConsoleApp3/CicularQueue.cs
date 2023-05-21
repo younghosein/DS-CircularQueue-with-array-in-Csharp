d75e2ecf1b390d4094929d1f2ec3b92489512202
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-21 16:26:04 +0330

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/CicularQueue.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class CicularQueue
+    {
+        private int[] ele;
+        private int front;
+        private int rear;
+        private int max;
+        private int count;
+
+        public CicularQueue(int size)
+        {
+            ele = new int[size];
+            front = 0;
+            rear = -1;
+            max = size;
+            count = 0;
+        }
+
+        public void insert(int item)
+        {
+            if (count == max)
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            else
+            {
+                rear = (rear + 1) % max;
+                ele[rear] = item;
+
+                count++;
+            }
+        }
+
+        public void delete()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Console.WriteLine("deleted element is: " + ele[front]);
+
+                front = (front + 1) % max;
+
+                count--;
+            }
+        }
+
+        public void printQueue()
+        {
+            int i = 0;
+            int j = 0;
+
+            if (count == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            else
+            {
+                for (i = front; j < count;)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]: " + ele[i]);
+
+                    i = (i + 1) % max;
+                    j++;
+
+                }
+            }
+        }
+        public void AvgMinQueue()
+        {
+            int i = 0;
+            int j = 0;
+            int s = 0;
+            if (count == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            else
+            {
+                if (ele[i] < 0)
+                {
+                    for (i = front; j < count;)
+                    {
+                        Console.WriteLine((s += ele[i]) / count);
+
+                        i = (i + 1) % max;
+                        j++;
+
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            CicularQueue Q = new CicularQueue(5);
+
+            Q.insert(10);
+            Q.insert(20);
+            Q.insert(30);
+            Q.insert(40);
+            Q.insert(50);
+
+            Console.WriteLine("Items are : ");
+            Q.printQueue();
+
+            Q.delete();
+            Q.delete();
+
+            Q.insert(60);
+            Q.insert(70);
+
+            Console.WriteLine("Items are : ");
+            Q.printQueue();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
